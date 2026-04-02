548666ddd705106b5ba4232a17a4f50f5bcaf94a
Q <143280874+Q-bran@users.noreply.github.com>
2026-04-02 13:52:16 +0200

    init commit

---
+++ src/test/java/nl/maastrichtuniversity/fse/dacs/AppTest.java
@@ -0,0 +1,13 @@
+package nl.maastrichtuniversity.fse.dacs;
+
+import junit.framework.TestCase;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest extends TestCase
+{
+    public void testDeliberateFailure() {
+        assertTrue(false);
+    }
+}
